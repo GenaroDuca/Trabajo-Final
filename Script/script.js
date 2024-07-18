e8e0c26762b79354ee090c450d6c6d7f4e0ee302
Genaro_Duca <genaroduca10@gmail.com>
2024-07-17 22:16:50 -0300

    Actualizacion correción página contacto

--- Script/script.js
+++ Script/script.js
@@ -10,34 +10,16 @@ formulario.addEventListener("click", function(e){
     const sinEspacio = datosFormulario.trim();   
     const dataRecuperada = sinEspacio.split(" ");
 
-    console.log(dataRecuperada);
-
+    console.log(dataRecuperada)
     cerrarFormulario();
 })
 
-/*
 function cerrarFormulario() {
     let textoFormulario = document.querySelector(".formulario");
     textoFormulario.classList.toggle("ocultar");
 
     let respuestaFormulario = document.querySelector(".texto-formulario-enviado");
     respuestaFormulario.classList.toggle("ocultar");
-    
 }
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
