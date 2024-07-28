4c0f148666631ec0dc0b9ad2b5689b4996f0f3c0
Genaro_Duca <genaroduca10@gmail.com>
2024-07-28 04:43:40 -0300

    Optimización de código. Falta actualizar stock

--- Script/scriptComprar.js
+++ Script/scriptComprar.js
@@ -4,22 +4,22 @@
 let productosAlmacen = ["Fideos", "Arroz", "Huevos", "Carne"]
 let preciosAlmacen = [2000, 1800, 500, 10000];
 let stockAlmacen = [20, 10, 12, 5];
-let imagenesAlmacen = []
+let imagenesAlmacen = ["Media/fideos.jpeg", "Media/arroz.png", "Media/huevos.jpg", "Media/carne.jpg"]
 
 let productosBebidas = ["Coca Cola", "Sprite", "Fanta", "Agua"]
 let preciosBebidas = [3000, 1500, 1100, 1250];
 let stockBebidas = [50, 15, 2, 9];
-let imagenesBebidas = []
+let imagenesBebidas = ["Media/coca cola.webp", "Media/sprite.jpg", "Media/fanta.jpg", "Media/agua.jpg"]
 
 let productosLacteos = ["Leche", "Queso", "Yogurt", "Manteca"]
 let preciosLacteos = [2000, 8000, 5000, 4500];
 let stockLacteos = [15, 8, 20, 10];
-let imagenesLacteos = []
+let imagenesLacteos = ["Media/leche.jpeg", "Media/queso.jpeg", "Media/yogurt.jpg", "Media/manteca.webp"]
 
 let productosPanaderia = ["Pan", "Harina", "Prepizzas", "Medialunas"]
 let preciosPanaderia = [1000, 800, 3000, 6000];
 let stockPanaderia = [10, 22, 5, 10];
-let imagenesPanaderia = []
+let imagenesPanaderia = ["Media/pan.webp", "Media/harina.jpg", "Media/prepizza.webp", "Media/medialunas.jpeg"]
 
 let productos = productosAlmacen.concat(productosBebidas, productosLacteos, productosPanaderia);
 console.log(productos)
