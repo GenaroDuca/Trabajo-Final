c7b9e2228bc9160d987c9c1548bbed5309aa8902
Genaro_Duca <genaroduca10@gmail.com>
2024-07-29 20:25:44 -0300

    Script nuevo: infoProductos

---
+++ Script/infoProductos.js
@@ -0,0 +1,23 @@
+"use strict"
+
+// Informacion sobre los productos --------------------------------------------------------------------------
+let productosAlmacen = ["Fideos", "Arroz", "Huevos", "Carne"]
+let preciosAlmacen = [2000, 1800, 500, 1000];
+let stockAlmacen = [20, 10, 12, 5];
+let imagenesAlmacen = ["Media/fideos.jpeg", "Media/arroz.png", "Media/huevos.jpg", "Media/carne.jpg"]
+
+let productosBebidas = ["Coca Cola", "Sprite", "Fanta", "Agua"]
+let preciosBebidas = [3000, 1500, 1100, 1250];
+let stockBebidas = [50, 15, 2, 9];
+let imagenesBebidas = ["Media/coca cola.webp", "Media/sprite.jpg", "Media/fanta.jpg", "Media/agua.jpg"]
+
+let productosLacteos = ["Leche", "Queso", "Yogurt", "Manteca"]
+let preciosLacteos = [2000, 8000, 5000, 4500];
+let stockLacteos = [15, 8, 20, 10];
+let imagenesLacteos = ["Media/leche.jpeg", "Media/queso.jpeg", "Media/yogurt.jpg", "Media/manteca.webp"]
+
+let productosPanaderia = ["Pan", "Harina", "Prepizzas", "Medialunas"]
+let preciosPanaderia = [1000, 800, 3000, 6000];
+let stockPanaderia = [10, 22, 5, 10];
+let imagenesPanaderia = ["Media/pan.webp", "Media/harina.jpg", "Media/prepizza.webp", "Media/medialunas.jpeg"]
+
--- Script/scriptComprar.js
+++ Script/scriptComprar.js
@@ -1,26 +1,5 @@
 "use strict"
 
-// Informacion sobre los productos --------------------------------------------------------------------------
-let productosAlmacen = ["Fideos", "Arroz", "Huevos", "Carne"]
-let preciosAlmacen = [2000, 1800, 500, 10000];
-let stockAlmacen = [20, 10, 12, 5];
-let imagenesAlmacen = ["Media/fideos.jpeg", "Media/arroz.png", "Media/huevos.jpg", "Media/carne.jpg"]
-
-let productosBebidas = ["Coca Cola", "Sprite", "Fanta", "Agua"]
-let preciosBebidas = [3000, 1500, 1100, 1250];
-let stockBebidas = [50, 15, 2, 9];
-let imagenesBebidas = ["Media/coca cola.webp", "Media/sprite.jpg", "Media/fanta.jpg", "Media/agua.jpg"]
-
-let productosLacteos = ["Leche", "Queso", "Yogurt", "Manteca"]
-let preciosLacteos = [2000, 8000, 5000, 4500];
-let stockLacteos = [15, 8, 20, 10];
-let imagenesLacteos = ["Media/leche.jpeg", "Media/queso.jpeg", "Media/yogurt.jpg", "Media/manteca.webp"]
-
-let productosPanaderia = ["Pan", "Harina", "Prepizzas", "Medialunas"]
-let preciosPanaderia = [1000, 800, 3000, 6000];
-let stockPanaderia = [10, 22, 5, 10];
-let imagenesPanaderia = ["Media/pan.webp", "Media/harina.jpg", "Media/prepizza.webp", "Media/medialunas.jpeg"]
-
 let productos = productosAlmacen.concat(productosBebidas, productosLacteos, productosPanaderia);
 console.log(productos)
 let stocks = stockAlmacen.concat(stockBebidas, stockLacteos, stockPanaderia)
@@ -70,11 +49,13 @@ function llenarInformacionProductos(producto, precio, imagen, stock, seccion) {
         let inputCantidad = document.createElement("input")
         inputCantidad.setAttribute("type", "number")
         inputCantidad.setAttribute("placeholder", "Cantidad")
+        inputCantidad.setAttribute("min", "0")
+        inputCantidad.setAttribute("max", stock[i])
         inputCantidad.setAttribute("value", "0")
         inputCantidad.setAttribute("class", "input-cantidad")
+        inputCantidad.setAttribute("id", producto[i])
         contenedorSeccion.appendChild(inputCantidad)
 
-        /*
         // Crear stock
         let textoStock = document.createElement("p")
         textoStock.setAttribute("class", "stock-producto")
@@ -88,7 +69,6 @@ function llenarInformacionProductos(producto, precio, imagen, stock, seccion) {
         let numeroStockTexto = document.createTextNode(stock[i])
         numeroStock.appendChild(numeroStockTexto)
         contenedorSeccion.appendChild(numeroStock)
-        */
        
         // Crea input de agregar
         let inputAgregar = document.createElement("input")
@@ -126,7 +106,7 @@ for (let i = 0; i < productos.length; i++) {
             btnAgregar.setAttribute("disabled", "true")
 
             // Actualizacion de stock (HACER)
-            
+
             // Agregar productos en factura
             let seccionFactura = document.getElementById("productos-factura")
             let productoEnFactura = document.createElement("p")
